08f6d1abfa51090518e6099afc946e45e288c4f8
ismailkavak <118558392+ismailkavak@users.noreply.github.com>
2023-06-17 16:28:28 +0300

    www.patika.dev

---
+++ LOOPS-Projects/loop14/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100:");
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
